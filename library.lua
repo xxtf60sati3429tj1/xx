4526ace3bd4639573e1c1c8a540a852f293862ee
xxtf60sati3429tj1 <scareisthemove@gmail.com>
2026-07-18 08:39:20 -0700

    library.lua

--- library.lua
+++ library.lua
@@ -261,10 +261,7 @@ do
             return Font.new(getcustomasset(`{Library.Directory .. Library.Folders.Assets}/{Name}.font`))
         end
 
-        Library.Font = CustomFont:New("SmallestPixel7", 400, "Regular", {
-            Id = "SmallestPixel7",
-            Url = "https://github.com/sametexe001/luas/raw/refs/heads/main/smallest_pixel-7.ttf"
-        })
+        Library.Font = Enum.Font.GothamLight
     end
 
     Library.Exit = function(Self)
